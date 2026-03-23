f36e58ef2c2c07de649dd2f6a3193fae4acd7f98
Kauan Damasceno <rm573727@fiap.com.br>
2026-03-23 19:00:53 -0300

    Add files via upload

---
+++ Aula 1/main.c
@@ -0,0 +1,42 @@
+// importações
+# include <stdio.h>
+
+// variáveis e funções
+
+// saída
+int main (){
+    
+    printf("Bom dia em Cs\n");
+    printf("Aprendendo a linguagem C\n");
+    return 0;
+}
+
+// cls {limpa terminal}
+
+// gcc {comando} main.c {nome do arquivo} -o {estrutura em forma de executável} programa {nome do executável}
+// ./programa {executa "programa"}
+
+//git init {inicializa o git}
+
+// git add . {adiciona os arquivos para o commit}
+// git add main.c {adiciona apenas o arquivo main.c para o commit}
+ 
+// -m {on a message}
+
+// git status {ver o status do que foi adicionado}
+
+// .gitignore {arquivo que tem a função de ignorar arquivos, como os .exe}
+        // *.exe        
+        // output/ 
+
+// git commit -m "Adicionando o tal do arquivo" {adiciona uma mensagem para o que foi adicionado com o add}
+
+// git rm --cached *exe {remove os arquivos .exe}
+
+// rmdir /s /q output {remove a pasta output}
+
+// git diff {ver o que mudaram}
+
+// git branch -M main
+
+// git remote add origin https://github.com/o-kauan/Aula.c-git.git
