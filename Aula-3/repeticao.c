40435c966d28c81829d60f615a94a61478881803
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-30 11:34:39 -0300

    repetição for

---
+++ Aula-3/repeticao.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+    for (int i = 0; i < 5; i++){
+        printf("%d\n", i + 1); // + 1 tira o 0        
+    }
+
+    for (int i = 1; i < 5; i++){
+        printf("%d\n", i);    
+    }
+
+
+    return 0;
+}
