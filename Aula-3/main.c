3070b3e245a553860cae346ca03c5407526203e8
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-30 11:19:37 -0300

    organização e input presenca

--- Aula-3/main.c
+++ Aula-3/main.c
@@ -4,20 +4,23 @@ int main (){
 
 // Tomada de decisão com estrutura If, Else If, e Else
     
-    int presenca = 80; // presença entre 0 e 100
-
     float nota_1, nota_2;
-
+    
     printf ("------------------------\n");
     printf ("CALCULADORA DE APROVACAO\n");
     printf ("------------------------\n");
+    
     printf ("Digite sua primeira nota: \n");
-    scanf("%f", &nota_1);
+    scanf("%f", &nota_1); // pergunta para o usuário
     printf ("Digite sua segunda nota: \n");
     scanf("%f", &nota_2);
-
     float media = (nota_1 + nota_2) / 2;
 
+    int presenca;
+    printf ("Digite sua presença entre 0 e 100: \n");
+    scanf("%d", &presenca);
+
+    
     if (media >= 9 && presenca >= 75){
         printf("Muito bom e Aprovado!\n");
     }else if (media >= 6 && presenca >= 75){
