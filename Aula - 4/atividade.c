9b9508ef67bcc17757d0a46faff77821f4bb985e
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-06 11:38:23 -0300

    causando erro na atv

--- Aula - 4/atividade.c
+++ Aula - 4/atividade.c
@@ -1,8 +1,19 @@
 # include <stdio.h>
 
 int main (){
+
+int numero;
 // usuário tem que digitar um valor
 printf("Digite um numero para ser somado ou digite o caracter s para sair: ");
+scanf("%d", numero);
+
+// lógica dp loop e soma
+char saida_loop;
+while (saida_loop != 's' || saida_loop != 'S'){
+    printf("Digite um numero para ser somado ou digite o caracter s para sair: ");
+    scanf("%c,", saida_loop);
+
+}
 
 return 0;
 }
