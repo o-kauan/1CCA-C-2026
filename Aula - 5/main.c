30a9e52908b1cdfd3c9a6a6575ee9d59a4e1aa71
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-13 11:42:05 -0300

    add funções (erro)

--- Aula - 5/main.c
+++ Aula - 5/main.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 
-int main (){
-
-    int opcao, presenca;
-    float nota_1, nota_2, media;
-
+void menu(){    // função que só exibe (retorna vazio)
     printf("--- MENU ---\n");
     printf("1 - MEDIA DO ALUNO\n");
     printf("2 - PRESENCA DO ALUNO\n");
-
     printf("Digite a opcao desejada: ");
-    scanf("%d", &opcao);
+    
+}
 
+    float mean(float nota_1, float nota_2){ // função cal média (retorna a mean (float))
+        return (nota_1 + nota_2)/2;
+    }
+
+int main (){
+
+    int opcao, presenca;
+    float nota_1, nota_2;
+
+    menu();
+    scanf("%d", &opcao);
+    
     switch (opcao){
         case 1: 
         printf("\n--- MEDIA DO ALUNO ---\n");
@@ -20,13 +28,14 @@ int main (){
         scanf("%f", &nota_1);
         printf("Digite sua segunda nota: \n");
         scanf("%f", &nota_2);
-        media = (nota_1 + nota_2)/2;
+        float media = mean(nota_1,nota_2);
         printf("A media final do aluno: %.2f\n", media);
 
         break;
 
         case 2: 
         printf("\n--- PRESENCA DO ALUNO ---\n");
+
         printf("Digite a presenca do aluno (0-100): \n");
         scanf("%d", &presenca);
 
